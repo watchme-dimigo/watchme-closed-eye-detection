0a65d652038270c82b6b518cb6d94bcb740776de
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-04-26 08:08:19 +0900

    Add build script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from cx_Freeze import setup, Executable
+
+base = None
+
+executables = [Executable('main.py', base=base)]
+
+packages = ['idna']
+options = {
+    'build_exe': {
+        'packages': packages,
+    },
+}
+
+setup(
+    name='watchmaker',
+    options=options,
+    version='0.0.1',
+    description='',
+    executables=executables
+)
+
+# python3 setup.py build
