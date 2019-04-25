a09ce9ab636ca4ab4429e9c84926c3fbe4e6f2ee
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-04-25 19:35:53 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+import random
+from time import sleep
+
+def randoutput():
+    return json.dumps({
+        'blink': random.getrandbits(1),
+        'track': [
+            random.randint(0, 1920),
+            random.randint(0, 1080)
+        ]
+    })
+
+while 1:
+    print(randoutput())
+    sleep(0.2 +  random.uniform(-0.1, 0.1))
